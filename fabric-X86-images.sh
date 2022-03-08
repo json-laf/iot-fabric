26f8af36942c0acd1984b09c6c67a8897400bc9a
json-laf <83752162+json-laf@users.noreply.github.com>
2022-03-08 17:29:01 +0800

    Add files via upload

---
+++ fabric-X86-images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# fabric-baseos
+docker pull hyperledger/fabric-baseos:2.3
+
+# fabric-tools
+docker pull hyperledger/fabric-tools:2.3
+
+# orderer
+docker pull hyperledger/fabric-orderer:2.3
+
+# peer
+docker pull hyperledger/fabric-peer:2.3
+
+# fabric-ccenv
+docker pull hyperledger/fabric-ccenv:2.3
---
+++ fabric-arm-images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# download the images and then tag them as default images
+
+# fabric-baseos
+docker pull ottoflaherty/fabric-baseos:arm64-2.3
+
+# fabric-tools
+docker pull ottoflaherty/fabric-tools:arm64-2.3
+
+# orderer
+docker pull ottoflaherty/fabric-orderer:arm64-2.3
+
+# peer
+docker pull ottoflaherty/fabric-peer:arm64-2.3
+
+# fabric-ccenv
+docker pull ottoflaherty/fabric-ccenv:arm64-2.3
